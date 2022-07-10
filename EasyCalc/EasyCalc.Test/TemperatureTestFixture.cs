17a9f9786a54a3181f78d58eb1ddb0395d628fd4
Ashiqur-Tanvir <mdashiqurrahman2010@gmail.com>
2022-07-11 00:03:54 +0200

    Added test fixture for Temperature class.

---
+++ EasyCalc/EasyCalc.Test/TemperatureTestFixture.cs
@@ -0,0 +1,25 @@
+﻿using EasyCalc.Contracts;
+using NUnit.Framework;
+
+namespace EasyCalc.Test
+{
+    [TestFixture]
+    public class TemperatureTestFixture
+    {
+        ITemperature _testee;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testee = new Temperature();
+        }
+
+        [Test]
+        public void CelsiusToFarenheit_ConvertsTemperatureCorrectly()
+        {
+            var temperatureInFarenheit = _testee.CelsiusToFarenheit(25.5);
+
+            Assert.AreEqual(77.9, temperatureInFarenheit, "Conversion with error.");
+        }
+    }
+}
