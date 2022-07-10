106b785f8bc13fffadac6d8162601ae45744fd2b
Ashiqur-Tanvir <mdashiqurrahman2010@gmail.com>
2022-07-10 22:01:18 +0200

    Created a solution and project for EasyCalc Application. - implemented ITemperature Interface.

---
+++ EasyCalc/EasyCalc/Contracts/ITemperature.cs
@@ -0,0 +1,15 @@
+﻿namespace EasyCalc.Contracts
+{
+    /// <summary>
+    /// Interface for temparature conversion.
+    /// </summary>
+    public interface ITemperature
+    {
+        /// <summary>
+        /// Converts temperature from celsius to farenheit.
+        /// </summary>
+        /// <param name="temperature">Temperature in celsius.</param>
+        /// <returns>Temperature in farenheit.</returns>
+        double CelsiusToFarenheit(double temperature); 
+    }
+}
---
+++ EasyCalc/EasyCalc/Temperature.cs
@@ -0,0 +1,21 @@
+﻿using EasyCalc.Contracts;
+
+namespace EasyCalc
+{
+    /// <inheritdoc cref="ITemperature"/>
+    public class Temperature : ITemperature
+    {
+        /// <inheritdoc cref="ITemperature.CelsiusToFarenheit"/>
+        public double CelsiusToFarenheit(double temperature)
+        {
+            var temperatureInFarenheit = (temperature * 1.8) + 32;
+
+            if (temperatureInFarenheit >  70) 
+            {
+                Console.WriteLine("Heute ist sehr warm!");
+            }
+
+            return temperatureInFarenheit;
+        }
+    }
+}
